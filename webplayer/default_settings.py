093747d17b269dcf71fdb7a9495926f2fc117ec6
wielki.borsuk <wielki.borsuk@gmail.com>
2021-02-28 13:40:56 +0100

    change local database name

--- webplayer/default_settings.py
+++ webplayer/default_settings.py
@@ -5,6 +5,6 @@ DEBUG = True
 BASE_PATH = os.path.expanduser('~/music')
 BASE_URL = 'http://localhost'
 
-DB_FILE = './testdb.json'
+DB_FILE = './testdb.db'
 
 PODCAST_FILE = '~/somewhere/all_podcast_urls.yml'
