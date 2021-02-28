0eae0c15e349cfa50a7c5a6642e3c5ec2a9117de
wielki.borsuk <wielki.borsuk@gmail.com>
2021-02-28 13:42:37 +0100

    update setup.py for new release

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
         name='web-player-server',
-        version='0.0.1',
+        version='0.0.2',
         packages=find_packages(),
         include_package_data=True,
         zip_safe=False,
         install_requires=[
-            'flask', 'flask-cors', 'tinydb', 'pyyaml'
+            'flask', 'flask-cors', 'tinydb', 'pyyaml', 'sqlalchemy'
             ]
         )
